4d76fc4d1a586a7df612a74c3f7cf919ac4a01c7
resssoft <resssoft@bk.ru>
2022-04-26 01:32:31 +0700

    increment3 - fix var name to uppercase

--- internal/storages/ram/ram.go
+++ internal/storages/ram/ram.go
@@ -16,25 +16,25 @@ var data = struct {
 	CounterData: make(map[string]models.Counter),
 }
 
-type simpleRamStorage struct{}
+type simpleRAMStorage struct{}
 
 func New() interfaces.Storage {
-	return &simpleRamStorage{}
+	return &simpleRAMStorage{}
 }
 
-func (s *simpleRamStorage) SaveGuage(key string, val models.Gauge) {
+func (s *simpleRAMStorage) SaveGuage(key string, val models.Gauge) {
 	data.Lock()
 	data.GaugeData[key] = val
 	data.Unlock()
 }
 
-func (s *simpleRamStorage) SaveCounter(key string, val models.Counter) {
+func (s *simpleRAMStorage) SaveCounter(key string, val models.Counter) {
 	data.Lock()
 	data.CounterData[key] = val
 	data.Unlock()
 }
 
-func (s *simpleRamStorage) GetGuages() map[string]models.Gauge {
+func (s *simpleRAMStorage) GetGuages() map[string]models.Gauge {
 	result := make(map[string]models.Gauge)
 	data.Lock()
 	for k, v := range data.GaugeData {
@@ -44,7 +44,7 @@ func (s *simpleRamStorage) GetGuages() map[string]models.Gauge {
 	return result
 }
 
-func (s *simpleRamStorage) GetCounters() map[string]models.Counter {
+func (s *simpleRAMStorage) GetCounters() map[string]models.Counter {
 	result := make(map[string]models.Counter)
 	data.Lock()
 	for k, v := range data.CounterData {
@@ -54,7 +54,7 @@ func (s *simpleRamStorage) GetCounters() map[string]models.Counter {
 	return result
 }
 
-func (s *simpleRamStorage) IncrementCounter(key string, val models.Counter) {
+func (s *simpleRAMStorage) IncrementCounter(key string, val models.Counter) {
 	data.Lock()
 	if current, ok := data.CounterData[key]; ok {
 		data.CounterData[key] = val + current
@@ -64,7 +64,7 @@ func (s *simpleRamStorage) IncrementCounter(key string, val models.Counter) {
 	data.Unlock()
 }
 
-func (s *simpleRamStorage) GetCounter(key string) (models.Counter, error) {
+func (s *simpleRAMStorage) GetCounter(key string) (models.Counter, error) {
 	var err error = nil
 	data.Lock()
 	value, ok := data.CounterData[key]
@@ -75,7 +75,7 @@ func (s *simpleRamStorage) GetCounter(key string) (models.Counter, error) {
 	return value, err
 }
 
-func (s *simpleRamStorage) GetGuage(key string) (models.Gauge, error) {
+func (s *simpleRAMStorage) GetGuage(key string) (models.Gauge, error) {
 	var err error = nil
 	data.Lock()
 	value, ok := data.GaugeData[key]
