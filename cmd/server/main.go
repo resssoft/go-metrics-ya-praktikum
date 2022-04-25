829e53b1e2c6f5b2ffb00afa98304f611024d6b2
resssoft <resssoft@bk.ru>
2022-04-25 13:57:45 +0700

    increment2 - change server structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"github.com/resssoft/go-metrics-ya-praktikum/internal/handlers"
+	ramstorage "github.com/resssoft/go-metrics-ya-praktikum/internal/storages/ram"
 	"log"
 	"net/http"
-	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", save)
+	storage := ramstorage.New()
+	handler := handlers.NewMetricsSaver(storage)
+	http.HandleFunc("/update/", handler.SaveMetrics)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func save(rw http.ResponseWriter, req *http.Request) {
-	uriParts := strings.Split(req.URL.Path, "/")
-	if len(uriParts) < 5 {
-		rw.WriteHeader(http.StatusForbidden)
-		fmt.Fprintf(rw, "unsupported method")
-		return
-	}
-	dataType := uriParts[2]
-	dataName := uriParts[3]
-	dataValue := uriParts[4]
-	rw.WriteHeader(http.StatusNoContent)
-	fmt.Println("data type,name,value: ", dataType, dataName, dataValue)
 }
