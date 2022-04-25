f3e25a6e73e31b82e914bc663660a440c5012d05
resssoft <resssoft@bk.ru>
2022-04-25 23:30:45 +0700

    increment3 - add main page for server metrics

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -1,13 +1,23 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/resssoft/go-metrics-ya-praktikum/internal/interfaces"
 	"github.com/resssoft/go-metrics-ya-praktikum/internal/models"
+	"html/template"
 	"net/http"
 	"strconv"
 )
+
+const tmpDefault = `
+<html><body><table align="center">
+{{range .Values}}
+<tr><td> {{.Name}} </td><td> {{.Value}} </td></tr>
+{{end}}
+</table></body></html>
+`
 
 type MetricsSaver struct {
 	storage interfaces.Storage
@@ -76,6 +86,39 @@ func (ms *MetricsSaver) GetCounter(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(rw, "%v", val)
 }
 
+type tmpValue struct {
+	Name  string
+	Value string
+}
+
+type tmp struct {
+	Values []tmpValue
+}
+
 func (ms *MetricsSaver) GetAll(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(rw, "%v", 1)
+	guages := ms.storage.GetGuages()
+	counters := ms.storage.GetCounters()
+	var values []tmpValue
+	for key, value := range guages {
+		values = append(values, tmpValue{
+			key,
+			fmt.Sprintf("%v", value),
+		})
+	}
+	for key, value := range counters {
+		values = append(values, tmpValue{
+			key,
+			fmt.Sprintf("%v", value),
+		})
+	}
+	tmpData := tmp{values}
+	t := template.Must(template.New("").Parse(tmpDefault))
+	var tpl bytes.Buffer
+	err := t.Execute(&tpl, tmpData)
+	if err != nil {
+		rw.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(rw, "%v", err.Error())
+		return
+	}
+	fmt.Fprintf(rw, "%v", tpl.String())
 }
